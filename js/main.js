4fb0344e600e2d438f3fbcd3c531d772e0e9ac3f
tippstar <benno.kramer@zalando.de>
2014-05-28 17:18:36 +0200

    fixed api cross domain request

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var myApp=angular.module("myApp",[]);
 myApp.controller("MyCtrl",['$scope','$http','$window',function($scope,$http,$window){  
   
   $http({
-    url : 'http://footballdb.herokuapp.com/api/v1/event/world.2014/teams',
+    url : 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/teams',
     method : "GET"    
   }).then(function(response){
     $scope.data=response.data;
