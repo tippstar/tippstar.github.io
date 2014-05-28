23a85767fc26986b8e67e64ef70945c7d683fed2
tippstar <benno.kramer@zalando.de>
2014-05-28 15:53:49 +0200

    changed controller name

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function Hello($scope, $http) {
+function getData($scope, $http) {
     $http.get('http://footballdb.herokuapp.com/api/v1/event/world.2014/teams').
     success(function(data) {
         $scope.data = data;
