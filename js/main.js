c4633494084e0d1130d22af5ab8a2dd3952354ce
tippstar <benno.kramer@zalando.de>
2014-05-28 17:33:09 +0200

    yesssa

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-var myApp=angular.module("myApp",[]);
+var myApp = angular.module("myApp", []);
 
-myApp.controller("MyCtrl",['$scope','$http','$window',function($scope,$http,$window){  
-  
-  $http({
-    url : 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/teams',
-    method : "GET"    
-  }).then(function(response){
-    $scope.data=response.data;
-    console.log($scope.data);
-  });
-  
-}]);
+myApp.controller("TeamCtrl", ['$scope', '$http', '$window',
+    function($scope, $http, $window) {
+
+        $http({
+            url: 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/teams',
+            method: "GET"
+        }).then(function(response) {
+            $scope.teams = response.data;
+            console.log($scope.teams);
+        });
+
+    }
+]);
+
+myApp.controller("AllRoundsCtrl", ['$scope', '$http', '$window',
+    function($scope, $http, $window) {
+
+        $http({
+            url: 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/rounds',
+            method: "GET"
+        }).then(function(response) {
+            $scope.rounds = response.data;
+            console.log($scope.rounds);
+        });
+
+    }
+]);
