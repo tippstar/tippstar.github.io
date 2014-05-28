6d8f4f24d832c4e14c4f6387f907459dfdbb4edb
tippstar <benno.kramer@zalando.de>
2014-05-28 18:01:42 +0200

    test2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("TeamCtrl", ['$scope', '$http', '$window',
     function($scope, $http, $window) {
-
         $http({
             url: 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/teams',
             method: "GET"
@@ -10,13 +9,11 @@ myApp.controller("TeamCtrl", ['$scope', '$http', '$window',
             $scope.teams = response.data;
             console.log($scope.teams);
         });
-
     }
 ]);
 
 myApp.controller("AllRoundsCtrl", ['$scope', '$http', '$window',
     function($scope, $http, $window) {
-
         $http({
             url: 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/rounds',
             method: "GET"
@@ -24,6 +21,17 @@ myApp.controller("AllRoundsCtrl", ['$scope', '$http', '$window',
             $scope.rounds = response.data;
             console.log($scope.rounds);
         });
+    }
+]);
 
+myApp.controller("AllGamesCtrl", ['$scope', '$http', '$window',
+    function($scope, $http, $window) {
+        $http({
+            url: 'http://www.corsproxy.com/footballdb.herokuapp.com/api/v1/event/world.2014/round/1',
+            method: "GET"
+        }).then(function(response) {
+            $scope.playday = response.data;
+            console.log($scope.playday);
+        });
     }
 ]);
