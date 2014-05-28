cb1e0cded06d8f445019eba70da840364a3a6bb7
tippstar <benno.kramer@zalando.de>
2014-05-28 17:03:27 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-function getData($scope, $http) {
-    $http.get('http://footballdb.herokuapp.com/api/v1/event/world.2014/teams').
-    success(function(data) {
-        $scope.data = data;
-    });
-}
+var myApp=angular.module("myApp",[]);
+
+myApp.controller("MyCtrl",['$scope','$http','$window',function($scope,$http,$window){  
+  
+  $http({
+    url : 'http://footballdb.herokuapp.com/api/v1/event/world.2014/teams',
+    method : "GET"    
+  }).then(function(response){
+    $scope.data=response.data;
+  });
+  
+}]);
